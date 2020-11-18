106346825bf0bb52bed720df9093e181abab14d2
flis_ann <ann.flismark@gmail.com>
2020-11-18 16:13:09 +0100

    Test for retrieving a collection of PerformanceData objects if the user makes a request, testing for the right response

--- spec/requests/api/v1/performance_data_spec.rb
+++ spec/requests/api/v1/performance_data_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe Api::V1::PerformanceDataController, type: :request do
-  let(:user) { create(:user) }
+  let(:user) { create(:user, email:"test@test.com") }
   let(:credentials) { user.create_new_auth_token }
   let(:headers) { { HTTP_ACCEPT: "application/json" }.merge!(credentials) }
 
@@ -23,6 +23,33 @@ RSpec.describe Api::V1::PerformanceDataController, type: :request do
       expect(entry.data).to eq "message" => "Average"
     end
   end
+
+  describe 'GET /api/v1/performance_data' do
+    let!(:existing_entries) do
+      5.times do
+        create(:performance_data,
+        data: { message: 'Average' },
+        user: user)
+      end
+    end
+    
+    before do
+      get '/api/v1/performance_data', headers: headers
+    end
+
+    it 'return a 200 response status' do
+      expect(response).to have_http_status 200
+    end
+
+    it 'returns a collection of performance data' do
+      expect(response_json['entries'].count).to eq 5
+    end
+  end
+end
+
+
+
+
   # describe "without user credentials information " do
   #   before do
   #     post "/api/v1/performance_data",
@@ -43,4 +70,4 @@ RSpec.describe Api::V1::PerformanceDataController, type: :request do
   #     expect(entry.data).to eq "message" => "Bad"
   #   end
   # end
-end
+
