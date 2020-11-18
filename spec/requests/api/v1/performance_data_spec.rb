1c6a332b1f00ec661ffaa0d6c9cea5d51e590e61
Kyungin Na <KyunginNa@1.local>
2020-11-18 16:00:23 +0100

    trying to add sad paths to the test file.

--- spec/requests/api/v1/performance_data_spec.rb
+++ spec/requests/api/v1/performance_data_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Api::V1::PerformanceDataController, type: :request do
   let(:user) { create(:user) }
   let(:credentials) { user.create_new_auth_token }
-  let(:headers) { { HTTP_ACCEPT: 'application/json' } .merge!(credentials) }
+  let(:headers) { { HTTP_ACCEPT: "application/json" }.merge!(credentials) }
 
   describe "POST /api/v1/performance_data" do
     before do
@@ -23,4 +23,24 @@ RSpec.describe Api::V1::PerformanceDataController, type: :request do
       expect(entry.data).to eq "message" => "Average"
     end
   end
+  # describe "without user credentials information " do
+  #   before do
+  #     post "/api/v1/performance_data",
+  #          params: {
+  #            performance_data: {
+  #              data: { message: "Bad" },
+  #            },
+  #          },
+  #          headers: headers
+  #   end
+
+  #   it "returns 401 response status" do
+  #     expect(response).to have_http_status 401
+  #   end
+
+  #   it "fails to create a data entry" do
+  #     entry = PerformanceData.last
+  #     expect(entry.data).to eq "message" => "Bad"
+  #   end
+  # end
 end
